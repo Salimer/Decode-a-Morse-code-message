21469b773bf8e3d5b2f5fab8c0bd59d1ac5183c6
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-19 18:11:03 +0300

    Fix linters

--- Challenge.rb
+++ Challenge.rb
@@ -1,36 +1,35 @@
 $morse_letters = {
-  ".-" => "A", "-..." => "B", "-.-." => "C",
-  "-.." => "D", "."     => "E", "..-."  => "F",
-  "--." => "G", "...."  => "H", ".."    => "I",
-  ".---" => "J", "-.-" => "K", ".-.." => "L",
-  "--" => "M", "-." => "N", "---" => "O",
-  ".--." => "P", "--.-" => "Q", ".-." => "R",
-  "..." => "S", "-" => "T", "..-" => "U",
-  "...-" => "V", ".--"   => "W", "-..-" => "X",
-  "-.--" => "Y", "--.."  => "Z"
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.'     => 'E', '..-.'  => 'F',
+  '--.' => 'G', '....'  => 'H', '..'    => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--'   => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..'  => 'Z'
 }
 
 def decode_char(morse_code)
-  letter = $morse_letters[morse_code]
-  letter
+  $morse_letters[morse_code]
 end
 
 def decode_word(morse_code)
-  decoded_word = ""
-  chars = morse_code.split(" ")
+  decoded_word = ''
+  chars = morse_code.split
   chars.each do |char|
-    decoded_word += decode_char(char);
+    decoded_word += decode_char(char)
   end
   decoded_word
 end
 
 
 # Example usage
-char = decode_char(".-")
+char = decode_char('.-')
 puts char
 
-word = decode_word("-- -.--")
+word = decode_word('-- -.--')
 puts word
 
-message = decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+message = decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 puts message
